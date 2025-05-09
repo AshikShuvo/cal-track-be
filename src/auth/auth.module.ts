d97a4de03d4e246339756b808c94afa971afc0a3
AshikShuvo <ashikshuvo1996@gmail.com>
2025-05-09 15:18:40 +0600

    chore: add config files and update dependencies for auth module - add JWT config, update env example, add required packages

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { PasswordService } from './services/password.service';
 import { RegistrationService } from './services/registration.service';
+import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { FacebookStrategy } from './strategies/facebook.strategy';
+import jwtConfig from '../config/jwt.config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule.forFeature(jwtConfig)],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: {
+          expiresIn: configService.get<string>('jwt.expiresIn'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [PasswordService, RegistrationService],
-  exports: [PasswordService],
+  providers: [
+    PasswordService,
+    RegistrationService,
+    AuthService,
+    JwtStrategy,
+    GoogleStrategy,
+    FacebookStrategy,
+  ],
+  exports: [PasswordService, AuthService],
 })
 export class AuthModule {} 
